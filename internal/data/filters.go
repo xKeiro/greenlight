64932677cfce2aba452ba6fad9e1847652a7a7a9
xKeiro <nkevin@outlook.hu>
2023-10-05 13:00:53 +0200

    Update movie listing route to include metadata

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"math"
 	"strings"
 
 	"github.com/xKeiro/greenlight/internal/validator"
@@ -11,6 +12,14 @@ type Filters struct {
     PageSize int
     Sort     string
 	SortSafelist []string
+}
+
+type Metadata struct {
+    CurrentPage  int `json:"current_page,omitempty"`
+    PageSize     int `json:"page_size,omitempty"`
+    FirstPage    int `json:"first_page,omitempty"`
+    LastPage     int `json:"last_page,omitempty"`
+    TotalRecords int `json:"total_records,omitempty"`
 }
 
 func (f Filters) sortColumn() string {
@@ -45,4 +54,18 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
 	v.Check(f.PageSize <= 100, "page_size", "must not be greater than 20")
 	v.Check(validator.PermittedValue(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
+}
+
+func calculateMetadata(totalRecords, page, pageSize int) Metadata {
+    if totalRecords == 0 {
+        return Metadata{}
+    }
+
+    return Metadata{
+        CurrentPage:  page,
+        PageSize:     pageSize,
+        FirstPage:    1,
+        LastPage:     int(math.Ceil(float64(totalRecords) / float64(pageSize))),
+        TotalRecords: totalRecords,
+    }
 }
